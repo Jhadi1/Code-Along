c4868dc08e947f60b30a72ec9351050db9015187
jhadi1 <jayhad662@gmail.com>
2020-02-04 15:00:21 -0500

    final commit in class

---
+++ codealong.js
@@ -0,0 +1,18 @@
+function printPrimes(){
+    for (var i = 0; i < 1000; i++){
+    if(i>= 0 && i<=3){
+        console.log(i);
+    } else if(i % 2 != 0 || i % 3 !=0){
+        //the number is less than 3 and is odd and isnt divisible by 3
+        var j = 5;
+        while(j*j <=i){
+            if(i%j=== 0 || i% (j+2)===0){
+                return false;
+            }
+            j +=6;
+            console.log(i);
+        }
+    }
+    }
+}
+printPrimes();
